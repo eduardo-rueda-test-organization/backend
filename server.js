90d5d53deb3fa667d00475882c42059fcfc59c0b
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2025-01-31 14:09:26 +0100

    Update the server

--- server.js
+++ server.js
@@ -3,31 +3,34 @@ const http = require('http');
 const server = http.createServer((req, res) => {
 
 	res.setHeader('Access-Control-Allow-Origin', '*'); // Or specify your frontend's origin
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Specify allowed methods
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Specify allowed headers (if any)
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Specify allowed methods
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Specify allowed headers (if any)
 
 	const url = req.url;
 
-    // Set the Content-Type header to indicate JSON if you intend to send JSON.
-    res.setHeader('Content-Type', 'text/plain'); // Or 'application/json' if sending JSON
+	// Set the Content-Type header to indicate JSON if you intend to send JSON.
+	res.setHeader('Content-Type', 'text/plain'); // Or 'application/json' if sending JSON
 
-    let message = ""; // Initialize a variable to hold the message
+	let message = ""; // Initialize a variable to hold the message
 
-    switch (url) {
-        case "/home":
-            message = "Welcome!";
-            break;
-        case "/user":
-            message = "You are a person!";
-            break;
-        case "/test":
-            message = "The test was successful!";
-            break;
-        default:
-            message = "Hello World!";
-    }
+	switch (url) {
+		case "/home":
+			message = "Welcome!";
+			break;
+		case "/user":
+			message = "You are a person!";
+			break;
+		case "/test":
+			message = "The test was successful!";
+			break;
+		case "/render":
+			message = "The render app works!";
+			break;
+		default:
+			message = "Hello World!";
+	}
 
-    res.end(message); 
+	res.end(message);
 })
 
 const PORT = process.env.PORT || 3000;
